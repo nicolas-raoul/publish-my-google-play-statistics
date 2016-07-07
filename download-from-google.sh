245797508eea61db2c929e5d278cf7d9748d2798
Nicolas Raoul <nicolas.raoul@gmail.com>
2016-07-07 13:49:22 +0900

    Working script

---
+++ download-from-google.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Download Google Play statistics
+
+GCS=/home/nico/p/google-cloud-sdk
+APP=01688209299889066540
+OUTPUT=/home/nico/src/google-play-statistics/AnkiDroid/
+
+$GCS/bin/gcloud auth login # Launches OAuth authentication in your browser. Log in with the Google account associated with your Google Play account
+
+# Remove previous data
+# TODO Only delete incomplete months and only download new data
+mv $OUTPUT/* /tmp # Replace with rm -rf if you are confident that your $OUTPUT variable is correctly set.
+
+# Download
+$GCS/bin/gsutil -m cp -R gs://pubsite_prod_rev_$APP/stats/installs $OUTPUT
+$GCS/bin/gsutil -m cp -R gs://pubsite_prod_rev_$APP/stats/ratings $OUTPUT
+$GCS/bin/gsutil -m cp -R gs://pubsite_prod_rev_$APP/stats/crashes $OUTPUT
+$GCS/bin/gsutil -m cp -R gs://pubsite_prod_rev_$APP/stats/gcm $OUTPUT
